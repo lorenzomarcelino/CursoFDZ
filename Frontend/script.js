97a703c6aa77da0d812d44f83aa869e8d3cc0439
LorenzoMM <Lorenzommarcelino@gmail.com>
2023-12-11 08:57:58 -0300

    new version

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,13 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const baseUrl = 'http://localhost:3000'; // Ajuste para a URL do seu servidor backend
+    const baseUrl = 'http://localhost:3000';
     let cursos = [];
+
+    window.mostrarProgresso = function() {
+        mostrarSeção('progressoContainer');
+        exibirProgresso();
+    };
+
+    window.mostrarCursos = function() {
+        mostrarSeção('cursosContainer');
+        exibirCursos();
+    };
+
+    window.adicionarCurso = function() {
+        mostrarSeção('adicionarCursoContainer');
+        // Implemente a lógica para adicionar curso aqui
+    };
+
+    window.adicionarAula = function() {
+        mostrarSeção('adicionarAulaContainer');
+        // Implemente a lógica para adicionar aula aqui
+    };
+
+    function mostrarSeção(seção) {
+        const todasSecoes = ['progressoContainer', 'cursosContainer', 'moduloContainer', 'aulaContainer', 'adicionarCursoContainer', 'adicionarAulaContainer'];
+        todasSecoes.forEach(id => {
+            document.getElementById(id).style.display = (id === seção) ? 'block' : 'none';
+        });
+    }
 
     function carregarCursos() {
         fetch(`${baseUrl}/dados`)
             .then(response => response.json())
             .then(dados => {
                 cursos = dados;
-                exibirCursos();
+                mostrarProgresso();
             })
             .catch(error => {
                 console.error('Erro ao carregar cursos:', error);
@@ -15,87 +42,155 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function exibirCursos() {
-        const moduloContainer = document.getElementById('moduloContainer');
-        moduloContainer.innerHTML = ''; // Limpar conteúdo
-        // Supondo que cada curso é único e é um objeto dentro de cursos
-        cursos.forEach(curso => {
-            const divModulo = document.createElement('div');
-            divModulo.className = 'modulo';
-            divModulo.textContent = curso['Módulo'];
-            divModulo.onclick = () => exibirAulas(curso['Módulo']);
-            moduloContainer.appendChild(divModulo);
+        const cursosContainer = document.getElementById('cursosContainer');
+        cursosContainer.innerHTML = '';
+        let cursosUnicos = new Set(cursos.map(curso => curso['Curso']));
+    
+        // Crie o contêiner de curso apenas uma vez
+        const divCursoContainer = document.createElement('div');
+        divCursoContainer.className = 'curso-container';
+    
+        cursosUnicos.forEach(nomeCurso => {
+            const divCurso = document.createElement('div');
+            divCurso.className = 'curso'; // Remova a classe 'curso-container' daqui
+    
+            const nomeCursoFormatado = nomeCurso.replace(/ /g, '');
+            const imgCurso = document.createElement('img');
+            imgCurso.src = `images/${nomeCursoFormatado}.png`;
+            imgCurso.alt = `Imagem do curso ${nomeCurso}`;
+            imgCurso.style.cursor = 'pointer';
+            imgCurso.addEventListener('click', () => exibirModulosOuAulas(nomeCurso));
+            divCurso.appendChild(imgCurso);
+    
+            // Adicione o curso ao contêiner de curso
+            divCursoContainer.appendChild(divCurso);
         });
+    
+        // Adicione o contêiner de curso ao cursosContainer
+        cursosContainer.appendChild(divCursoContainer);
+    }
+    
+    function exibirModulosOuAulas(nomeCurso) {
+        const aulasDoCurso = cursos.filter(curso => curso['Curso'] === nomeCurso);
+        if (aulasDoCurso.length > 0) {
+            const modulos = new Set(aulasDoCurso.map(aula => aula['Modulo']));
+            
+            // Se houver mais de um módulo, exiba os módulos
+            if (modulos.size > 1) {
+                exibirModulos(nomeCurso);
+            } else {
+                // Se houver apenas um módulo, exiba as aulas diretamente
+                const nomeModuloUnico = Array.from(modulos)[0];
+                exibirAulas(nomeCurso, nomeModuloUnico);
+            }
+        }
     }
 
-    function exibirAulas(nomeModulo) {
-        const aulaContainer = document.getElementById('aulaContainer');
+    function exibirModulos(nomeCurso) {
+        mostrarSeção('moduloContainer');
         const moduloContainer = document.getElementById('moduloContainer');
-        moduloContainer.style.display = 'none'; // Esconde módulos
-        aulaContainer.style.display = 'flex'; // Mostra aulas
+        moduloContainer.innerHTML = '';
+    
+        const modulosDoCurso = new Set(cursos.filter(curso => curso['Curso'] === nomeCurso).map(curso => curso['Modulo']));
+    
+        // Crie o contêiner de módulos apenas uma vez
+        const divModuloContainer = document.createElement('div');
+        divModuloContainer.className = 'curso-container'; // Use a mesma classe do contêiner de cursos
+    
+        modulosDoCurso.forEach(modulo => {
+            const divModulo = document.createElement('div');
+            divModulo.className = 'curso'; // Use a mesma classe dos cursos
+    
+            const nomeModuloFormatado = modulo.replace(/ /g, '');
+            const imgModulo = document.createElement('img');
+            imgModulo.src = `images/${nomeModuloFormatado}.png`;
+            imgModulo.alt = `Imagem do módulo ${modulo}`;
+            imgModulo.style.cursor = 'pointer';
+            imgModulo.addEventListener('click', () => exibirAulas(nomeCurso, modulo));
+            imgModulo.className = 'modulo'; // Adicione a classe 'modulo' às imagens dos módulos
+            divModulo.appendChild(imgModulo);
+    
+            // Adicione o módulo ao contêiner de módulos
+            divModuloContainer.appendChild(divModulo);
+        });
+    
+        // Adicione o contêiner de módulos ao móduloContainer
+        moduloContainer.appendChild(divModuloContainer);
+    }
 
-        const aulasDoModulo = cursos.filter(curso => curso['Módulo'] === nomeModulo);
-        aulaContainer.innerHTML = ''; // Limpar aulas anteriores
-        aulasDoModulo.forEach(aula => {
+    function exibirAulas(nomeCurso, nomeModulo) {
+        mostrarSeção('aulaContainer');
+        const aulaContainer = document.getElementById('aulaContainer');
+        aulaContainer.innerHTML = '';
+    
+        const linhaVertical = document.createElement('div');
+        linhaVertical.className = 'linha-vertical';
+        aulaContainer.appendChild(linhaVertical);
+    
+        const aulasDoModulo = cursos.filter(curso => curso.Curso === nomeCurso && curso.Modulo === nomeModulo);
+        
+        aulasDoModulo.forEach((aula, index) => {
             const divAula = document.createElement('div');
             divAula.className = 'aula';
+    
             divAula.innerHTML = `
-                <h4>${aula['Nome da Aula']}</h4>
-                <p>Andamento: ${aula['Minuto Parado']}</p>
-                <p>Assistida: ${aula['Assistida (Sim/Não)']}</p>
-                <button onclick="editarAula('${aula['Nome da Aula']}')">Editar</button>
+                <h3>${aula['Nome da Aula']}</h3>
+                <p>Duração: ${aula['Duração']}</p>
+                ${aula['Minuto Parado'] ? `<p>Parado em: ${aula['Minuto Parado']}</p>` : ''}
+                <button class="edit-btn" onclick="editarAula('${aula['Nome da Aula']}')">Editar</button>
             `;
+    
+            // O marcador da linha do tempo agora é parte da 'aula'
+            const bolinha = document.createElement('div');
+            bolinha.className = 'bolinha';
+            divAula.appendChild(bolinha);
+    
             aulaContainer.appendChild(divAula);
         });
+    
+        // Após adicionar todas as aulas, ajusta a altura da linha vertical
+        setTimeout(() => {
+            linhaVertical.style.height = `${aulaContainer.offsetHeight}px`;
+        }, 0);
     }
 
-    window.editarAula = function(nomeAula) {
-        const aulaParaEditar = cursos.find(aula => aula['Nome da Aula'] === nomeAula);
-        if (aulaParaEditar) {
-            const formHtml = `
-                <div class="editForm">
-                    <h2>Editando: ${nomeAula}</h2>
-                    <label for="assistida">Assistida (Sim/Não):</label>
-                    <select id="assistida" name="assistida">
-                        <option value="Sim" ${aulaParaEditar['Assistida (Sim/Não)'] === 'Sim' ? 'selected' : ''}>Sim</option>
-                        <option value="Não" ${aulaParaEditar['Assistida (Sim/Não)'] === 'Não' ? 'selected' : ''}>Não</option>
-                    </select>
-                    <label for="minutoParado">Minuto Parado:</label>
-                    <input type="text" id="minutoParado" name="minutoParado" value="${aulaParaEditar['Minuto Parado'] || ''}">
-                    <button onclick="salvarEdicao('${nomeAula}')">Salvar</button>
-                </div>
-            `;
-            document.getElementById('aulaContainer').innerHTML = formHtml;
-        }
-    };
+    function editarAula(nomeAula) {
+        // Implementação da função de editar aula
+        console.log('Editando aula:', nomeAula);
+        // Implemente a lógica de edição aqui
+    }
+    
+    function ajustarAlturaLinha() {
+        const linhaVertical = document.querySelector('.linha-vertical');
+        const aulaContainer = document.getElementById('aulaContainer');
+        linhaVertical.style.height = `${aulaContainer.offsetHeight}px`;
+        linhaVertical.style.top = '50%';
+        linhaVertical.style.transform = 'translateY(-50%)';
+    }
+    
+    // ... quando suas aulas forem adicionadas ...
+    ajustarAlturaLinha(); // Isso ajustará a altura da linha
+    
+    function exibirProgresso() {
+        const progressoContainer = document.getElementById('progressoContainer');
+        progressoContainer.innerHTML = '';
 
-    window.salvarEdicao = function(nomeAula) {
-        const aulaParaEditar = cursos.find(aula => aula['Nome da Aula'] === nomeAula);
-        if (aulaParaEditar) {
-            aulaParaEditar['Assistida (Sim/Não)'] = document.getElementById('assistida').value;
-            aulaParaEditar['Minuto Parado'] = document.getElementById('minutoParado').value;
-            
-            fetch(`${baseUrl}/atualizar`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(aulaParaEditar),
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Falha ao salvar a edição.');
-            })
-            .then(() => {
-                alert('Aula atualizada com sucesso!');
-                carregarCursos(); // Recarregar a lista de cursos
-            })
-            .catch(error => {
-                alert(error.message);
-            });
-        }
-    };
+        cursos.forEach(curso => {
+            if (Array.isArray(curso.aulas)) {
+                const aulasAssistidas = curso.aulas.filter(aula => aula['Assistida (Sim/Não)'] === 'Sim').length;
+                const totalAulas = curso.aulas.length;
+                const progresso = (aulasAssistidas / totalAulas * 100).toFixed(2);
 
-    carregarCursos(); // Iniciar carregamento dos cursos
+                const divProgresso = document.createElement('div');
+                divProgresso.className = 'progresso';
+                divProgresso.innerHTML = `
+                    <h3>${curso.nome}</h3>
+                    <p>Progresso: ${progresso}%</p>
+                `;
+                progressoContainer.appendChild(divProgresso);
+            } else {
+                console.log(`Curso ${curso.nome} não tem aulas definidas.`);
+            }});
+    }
+    carregarCursos();
 });
